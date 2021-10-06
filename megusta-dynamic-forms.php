c3312c1be01b5c08d977d00319496a76956b6313
Santiago Carmo <santiagocca@gmail.com>
2021-10-06 01:05:36 -0300

    adding plugin head

---
+++ megusta-dynamic-forms.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name:       Megusta Dynamic Forms
+ * Plugin URI:        https://megusta.co
+ * Description:       A toolkit to help you create any kind of form on your website. The best part, it's completely free ;)
+ * Version:           1.0.0
+ * Author:            Santiago Carmo <santiago@megusta.co>
+ * Author URI:        https://megusta.co
+ * License:           GPLv2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       megusta-dynamic-forms
+ * Domain Path:       /languages
+ * 
+ * @package MegustaDynamicForms
+ */
+
+
+defined( 'ABSPATH' ) || exit;
+
+require_once dirname(__FILE__) . '/config/megusta-dynamic-forms-constants.php';
