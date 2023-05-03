05196a3cc7b0e923a4312ec943b11ae30fc82dae
Santiago Carmo <santiagocca@gmail.com>
2023-05-03 00:26:15 -0300

    layout structure

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -7,7 +7,10 @@ function App() {
   return (
     <div className={css.app}>
         <Form />
-        
+        <div className={css['app__container']}>
+          <Box className={css['app__stage']}>asdasd</Box>
+          <Box className={css['app__controllers']}>asdasd</Box>
+        </div>
     </div>
   );
 }
--- admin/src/components/ui/Box/Box.tsx
+++ admin/src/components/ui/Box/Box.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import css from './Box.module.scss';
 import { Props } from '../../../models/Props.types';
 
-const Box: React.FC<Props> = (props) => {
-    return <div className={css.box}>{ props.children }</div>
+const Box: React.FC<{children:React.ReactNode, className?:string }> = (props) => {
+    const cssClasses = `${css.box} ${props.className}`
+    return (
+        <div className={cssClasses}>
+            { props.children }
+        </div>
+    );
 }
 
 export default Box;
