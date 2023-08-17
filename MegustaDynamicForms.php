1c24c6e8a76cd24ae67be485d705a40f7d4d8080
Santiago Carmo <santiagocca@gmail.com>
2023-08-17 18:46:32 -0300

    the frontend dev port was updated and react dev output was integrated with wordpress

--- MegustaDynamicForms.php
+++ MegustaDynamicForms.php
@@ -21,12 +21,14 @@ require_once ABSPATH . 'wp-admin/install-helper.php';
 class MegustaDynamicForms {
     const MDF_PLUGIN_VERSION = '1.0.0';
     const DB_OPTION_PLUGIN_VERSION = 'megusta-dynamic-forms-plugin-version';
+    const MDF_NONCE_KEY = 'dAYzcj:7yC$igWU4#9VEsbYcqKg4a&3PM7%KKH3tv@PzJ7mUm!gnkrRj';
 
     public static function init() {
         $api = new MDFApi();
         register_activation_hook(MDF_PLUGIN_FILE , [static::class, 'onActiveHandler']);
-        add_action('admin_menu', [static::class, 'enqueueScripts']);
+        add_action('admin_init', [static::class, 'enqueueAdminScripts']);
         add_action('admin_menu', [static::class, 'createAdminMenus']);
+        add_action('wp_enqueue_scripts', [static::class, 'enqueueFrontendScripts']);
     }
 
     public static function onActiveHandler() {
@@ -44,6 +46,11 @@ class MegustaDynamicForms {
     }
 
     public static function enqueueScripts() {
+        self::enqueueAdminScripts();
+        self::enqueueFrontendScripts();
+    }
+
+    public static function enqueueAdminScripts() {
         if(in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
             $jsLoadScript = 'http://localhost:3000/static/js/bundle.js';
             $cssLoadStyles = 'http://localhost:3000/static/css/bundle.css';
@@ -57,25 +64,19 @@ class MegustaDynamicForms {
         wp_enqueue_script('megusta_dynamic_forms_js_icons', 'https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js', '', mt_rand(10,1000), true);
     }
 
+    public static function enqueueFrontendScripts() {
+        if(in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
+            $jsLoadScript = 'http://localhost:3001/static/js/bundle.js';
+            $cssLoadStyles = 'http://localhost:3001/static/css/bundle.css';
+        } else {
+            $jsLoadScript = plugin_dir_url( __FILE__ ) . 'ghost-inspector.js'; //update this path
+            $cssLoadStyles = plugin_dir_url( __FILE__ ) . 'ghost-inspector.css'; //update this path
+        }
 
-    // public static function init() {
-    //     require dirname(MDF_PLUGIN_FILE) . '/api/MDFAdminController.php';
-
-    //     $formController = new MDFAdminController();
-
-    //     register_activation_hook(MDF_PLUGIN_FILE, ['MegustaDynamicForms', 'activateMegustaDynamicForms']);
-    //     register_deactivation_hook(MDF_PLUGIN_FILE, ['MegustaDynamicForms', 'deactivateMegustaDynamicForms']);
-    //     add_action('admin_menu', ['MegustaDynamicForms', 'createAdminMenus']);
-    // }
-    
-    // public static function activateMegustaDynamicForms() {
-    //     require_once dirname(MDF_PLUGIN_FILE) . '/data/MDFCreateDatabaseStructure.php';
-    //     if(class_exists('MDFCreateDatabaseStructure')) {
-    //         MDFCreateDatabaseStructure::generateDatabaseTables();
-    //     }
-    // }
-
-    // public static function deactivateMegustaDynamicForms() {
-
-    // }
+        wp_enqueue_style('megusta_dynamic_forms_css', $cssLoadStyles);
+        wp_enqueue_script('megusta_dynamic_forms_js', $jsLoadScript, '', mt_rand(10,1000), true);
+        wp_localize_script( 'Megusta_Dynamic_Forms_public', 'MDFPublic',[
+            'mdf_nonce' => wp_create_nonce(self::MEGUSTA_NONCE_KEY)
+        ]);
+    }
 }
